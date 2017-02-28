31c98c8e68705ec7391756ea5379d720a6421f70
Akihiro Makino <makino.aaa@gmail.com>
2017-03-01 03:24:22 +0900

    [fix]test

--- pyzaif/pyzaif.py
+++ pyzaif/pyzaif.py
@@ -9,6 +9,7 @@ import urllib
 from datetime import datetime
 from urllib.parse import urlencode
 
+
 class API(object):
     def __init__(self, api_key=None, api_secret=None):
         self.api_key = api_key
--- test/test_pyzaif.py
+++ test/test_pyzaif.py
@@ -1,10 +1,10 @@
 import pyzaif
 import unittest
-
+import os
 
 class TestPyzaif(unittest.TestCase):
     def setUp(self):
-        self.object = pyzaif.API(api_key="***", api_secret="***")
+        self.object = pyzaif.API(api_key=os.environ["ZAIF_API_KEY"], api_secret=os.environ["ZAIF_API_SECRET"])
 
     def test_request_api(self):
         self.object.request_api(endpoint="/trades/btc_jpy")
